7fd081ff71cbb4c7ca32023e2782af3449edcb6b
Shong Lee <Shonglee17@gmail.com>
2023-06-13 18:13:27 -0500

    added SQLite to project

---
+++ United Care Services/SQLite.swift
@@ -0,0 +1,20 @@
+//
+//  SQLite.swift
+//  United Care Services
+//
+//  Created by Shong Lee on 6/13/23.
+//
+
+import SwiftUI
+import SQLite
+//struct SQLite: View {
+//    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//    }
+//}
+//
+//struct SQLite_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SQLite()
+//    }
+//}
